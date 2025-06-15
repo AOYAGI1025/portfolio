76199dca700e1a80c5b73c5a7d436ede2937c185
AOYAGI1025 <153875179+AOYAGI1025@users.noreply.github.com>
2025-06-16 02:51:14 +0900

    Add files via upload

---
+++ Works/2023_TAKAOKA/js/08_slick.js
@@ -0,0 +1,13 @@
+	$(function(){
+		$(".slider").slick({
+			autoplay: true,
+			autoplaySpeed: 3000,
+			dots: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			infinite: true,
+			centerMode: true,
+			variableWidth: true,
+			centerPadding: '60px'
+		});
+	});
